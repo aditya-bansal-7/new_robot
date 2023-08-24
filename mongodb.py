104267ff6c0c0b1cf794c9a42dcdbaea363a4a61
bnslboy <adityabansalsafe@gmail.com>
2023-08-24 17:54:03 +0530

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -11,6 +11,7 @@ from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+ext ="hey"
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
